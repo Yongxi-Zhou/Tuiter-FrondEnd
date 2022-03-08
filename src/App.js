2f2697e15735817099caad3dd1c7eae62187830c
chow's outlook <chow971@outlook.com>
2022-03-07 19:52:57 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./styles.css";
+// import Navigation from "./components/navigation";
+// import Tuits from "./components/tuits";
+// import WhatsHappening from "./components/whats-happening/whats-happening";
+// import Bookmarks from "./components/bookmarks/bookmarks";
+import Tuiter from "./components/tuiter";
+//123123
+
+function App() {
+  return <Tuiter />;
+}
+
+export default App;
