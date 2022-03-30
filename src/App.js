8aa6bd6e1491e4526ae66857dd961b83f7ebf248
chow's outlook <chow971@outlook.com>
2022-03-30 13:34:59 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import logo from './logo.svg';
-import './styles.css';
-// import Navigation from "./components/navigation";
-// import Tuits from "./components/tuits";
-// import WhatsHappening from "./components/whats-happening/whats-happening";
-// import Bookmarks from "./components/bookmarks/bookmarks";
+import logo from "./logo.svg";
+import "./styles.css";
 import Tuiter from "./components/tuiter";
 
 function App() {
-  return (
-    <Tuiter/>
-  );
+  return <Tuiter />;
 }
 
 export default App;
